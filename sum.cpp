6c5c4f598b43a8472fb53aeae85a8d0970a66d3a
sunil sharma <sharmasunil51771@gmail.com>
2024-11-11 18:59:01 +0530

    first file sum program

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a,b,c;
+a=10;
+b=20;
+c=30;
+printf("enter the number of a");
+scanf("%d",&a);
+printf("enter the number of b");
+scanf("%d",&b);
+printf("enter the number of c");
+scanf("%d",&c);
+printf("sum=%d",a+b+c);
+getch();
+}
